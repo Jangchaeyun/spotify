a58e77bc30d3fa002714773840df7eb88e489816
zzangsally <zzangsally@naver.com>
2022-08-25 22:17:23 +0900

    ADD: Sidebar - content

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import Image from "next/image";
 
 function Sidebar() {
   return (
-    <div>
-      Siderbar
-    </div>
-  )
+    <section
+      className="fixed top-0 z-40 flex flex-col p-4 items-center bg-black 
+      w-[90px] h-screen space-y-8"
+    >
+      <Image
+        src="https://rb.gy/xkacau"
+        width={56}
+        height={56}
+        objectFit="contain"
+      />
+      <div className="text-white">
+        hello world
+      </div>
+    </section>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
