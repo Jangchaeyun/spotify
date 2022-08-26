9a6d7a2de70a277340acfa9cf9081b9b897f645a
zzangsally <zzangsally@naver.com>
2022-08-26 22:00:33 +0900

    ADD: Login - auth

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,23 @@
-function signin() {
+import Head from 'next/head';
+import Image from "next/image";
+
+function Signin() {
   return (
     <div>
-      
+      <Head>
+        <title>Login - Spotify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Image
+        src="https://rb.gy/y9mwtb"
+        height={250}
+        width={600}
+        objectFit="contain"
+        className="animate-pulse"
+      />
     </div>
   )
 }
 
-export default signin
+export default Signin
