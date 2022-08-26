55dfe6849db3f012818185d562c202a0f6ff1216
zzangsally <zzangsally@naver.com>
2022-08-26 18:23:49 +0900

    ADD: tailwind config and globals.css

--- components/Search.js
+++ components/Search.js
@@ -14,13 +14,12 @@ function Search({search, setSearch}) {
             />
             <div className="flex items-center divide-dotted divide-x-2 divide-[#333]">
                 <div className="flex space-x-2 pr-5">
-                    <button className="tag">Minimal</button>
-                    <button className="tag">House</button>
+                    <button className="tag">팝</button>                    
                     <button className="tag">Kpop</button>
                 </div>
                 <div className="flex items-center space-x-1.5 text-[#cecece] pl-4">
                     <MdOutlineShortText className="text-2xl animate-pulse"/>
-                    <span className='font-medium text-sm'>Filter</span>
+                    <span className='font-medium text-sm'>Filters</span>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,36 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    extend: {
+      screens: {
+        "2xl": "1500px",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0.5,
+          },
+        },
+      },
+      animation: {
+        pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+    },
+    fontFamily: {
+      body: ["Lato", "sans-serif"],
+      sans: ["ui-sans-serif", "system-ui"],
+    },
+  },
+  variants: {
     extend: {},
+    scrollbar: ["rounded"],
   },
   plugins: [
     require("tailwind-scrollbar-hide"),
     require("@tailwindcss/forms"),
     require("tailwind-scrollbar"),
   ],
-}
+};
