2483e86ab73b1a705120083ad432a5e23f94a8e5
zzangsally <zzangsally@naver.com>
2022-08-27 19:16:36 +0900

    ADD: Track - artist name and title

--- components/Track.js
+++ components/Track.js
@@ -1,3 +1,5 @@
+import { ImHeadphones } from "react-icons/im"
+
 function Track({ track, chooseTrack }) {
   return (
     <div className="flex items-center justify-between space-x-20 cursor-default hover:bg-white/10 py-2 px-4 rounded-lg group transition ease-out">
@@ -11,7 +13,16 @@ function Track({ track, chooseTrack }) {
             <h4 className="text-white text-sm font-semibold truncate w-[450px]">
                 {track.title}
             </h4>
-            <p className="text-[rgb(179,179,179)]">{track.artist}</p>
+            <p className="text-[rgb(179,179,179)] text-[13px] font-semibold group-hover:text-white">
+                {track.artist}
+            </p>
+        </div>
+      </div>
+
+      <div className="md:ml-auto flex items-center space-x-2.5">
+        <div>
+            <ImHeadphones className="text-lg" />
+            <h4>{track.popularity}</h4>
         </div>
       </div>
     </div>
