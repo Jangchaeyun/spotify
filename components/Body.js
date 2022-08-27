2dd61284d7d392183ad53f8d9e3c78d87c671fc7
zzangsally <zzangsally@naver.com>
2022-08-27 13:32:55 +0900

    ADD: Body - map songs

--- components/Body.js
+++ components/Body.js
@@ -1,6 +1,7 @@
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import Search from "./Search"
+import Search from "./Search";
+import Poster from "./Poster";
 
 function Body({ spotifyApi }) {
     const { data: session } = useSession();
@@ -64,7 +65,25 @@ function Body({ spotifyApi }) {
             <Search search={search} setSearch={setSearch}/>
             <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3
             xl-grid-cols-4 gap-x-4 gap-y-8 p-4">
-                {searchResults.length === 0 ? }
+                {searchResults.length === 0 
+                    ? newReleases
+                        .slice(0,4)
+                        .map((tack) => {
+                            <Poster 
+                                key={track.id}
+                                track={track} 
+                                // chooseTrack={chooseTrack}
+                            />
+                        })
+                    : searchResults
+                        .slice(0,4)
+                        .map((tack) => (
+                            <Poster 
+                                key={track.id}
+                                track={track} 
+                                // chooseTrack={chooseTrack}
+                            />
+                        ))}
             </div>
         </section>
     );
