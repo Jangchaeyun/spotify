30fe4fa0d1b50034a3dd21d3331ede3e7b7ccf21
zzangsally <zzangsally@naver.com>
2022-08-27 13:14:24 +0900

    ADD: Body - filter song result(search, new releases)

--- components/Body.js
+++ components/Body.js
@@ -7,7 +7,7 @@ function Body({ spotifyApi }) {
     const { accessToken }  = session;
     const [search, setSearch] = useState("");
     const [searchResults, setSearchResults] = useState([]);
-    const [newReleases, setNewRelease] = useState([]);
+    const [newReleases, setNewReleases] = useState([]);
 
     useEffect(() => {
         if (!accessToken) return;
@@ -20,22 +20,52 @@ function Body({ spotifyApi }) {
         if (!accessToken) return;
 
         spotifyApi.searchTracks(search).then((res) => {
-            setSearchResults(res.body.tracks.items.map((track) => {
-                return {
-                    id: track.id
-                }
-            }));
+            setSearchResults(
+                res.body.tracks.items.map((track) => {
+                    return {
+                        id: track.id,
+                        artist: track.artists[0].name,
+                        title: track.name,
+                        uri: track.uri,
+                        albumUrl: track.album.images[0].url,
+                        popularity: track.popularity,
+                    };
+                })
+            );
         })
     },[search, accessToken]);
 
     console.log(searchResults);
+
+    // New
+    useEffect(() => {
+        if (!accessToken) return;
+    
+        spotifyApi.getNewReleases().then((res) => {
+            setNewReleases(
+                res.body.albums.items.map((track) => {
+                    return {
+                        id: track.id,
+                        artist: track.artists[0].name,
+                        title: track.name,
+                        uri: track.uri,
+                        albumUrL: track.images[0].url,
+                        popularity: track.popularity,
+                    };
+                })
+            );
+        })
+    },[accessToken]);
+    
 
     return (
         <section
             className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
             <Search search={search} setSearch={setSearch}/>
             <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3
-            xl-grid-cols-4 gap-x-4 gap-y-8 p-4"></div>
+            xl-grid-cols-4 gap-x-4 gap-y-8 p-4">
+                {searchResults.length === 0 ? }
+            </div>
         </section>
     );
 }
