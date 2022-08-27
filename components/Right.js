7b8d61eb0d322e880debf72d67ad6822b2e72b44
zzangsally <zzangsally@naver.com>
2022-08-27 22:21:09 +0900

    ADD: Recentlt played container

--- components/Right.js
+++ components/Right.js
@@ -3,8 +3,34 @@ import { MdOutlineSettings } from "react-icons/md";
 import { BiBell } from "react-icons/bi";
 import { ViewGridIcon } from "@heroicons/react/solid";
 import Dropdown from './Dropdown';
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import RecentlyPlayed from './RecentlyPlayed';
 
-function Right() {
+function Right({ spotifyApi, chooseTrack }) {
+  const { data: session } = useSession();
+  const { accessToken } = session;
+  const [recentlyPlayed, setRecentlyPlayed] = useState([]);
+
+  // Recently Played Tracks...
+  useEffect(() => {
+    if (!accessToken) return;
+
+    spotifyApi.getMyRecentlyPlayedTracks({ limit: 20 }).then((res) => {
+      setRecentlyPlayed(
+        res.body.items.map(({ track }) => {
+          return {
+            id: track.id,
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: track.album.images[0].url,
+          };
+        })
+      );
+    });
+  }, [accessToken]);
+
   return (
     <section className="p-4 space-y-8 pr-8">
       <div className="flex space-x-2 items-center justify-between">
@@ -17,8 +43,24 @@ function Right() {
         {/* Profile */}
         <Dropdown />
       </div>
+      {/* Recently Played */}
+      <div className="bg-[#0d0d0d] border-2 border-[#262626] p-4 rounded-xl spce-y-4">
+        <div className="flex items-start justify-between">
+          <h4 className="text-white font-semibold text-sm">최근 감상한 곡</h4>
+          <ViewGridIcon className="text-[#686868] h-6"/>
+        </div>
+        <div className="space-y-4 overflow-y-scroll overflow-x-hidden h-[250px] md:h-[400px] scrollbar-hide">
+          {recentlyPlayed.map((track,index) => (
+            <RecentlyPlayed
+              key={index}
+              track={track}
+              chooseTrack={chooseTrack}
+            />
+          ))}
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Right
